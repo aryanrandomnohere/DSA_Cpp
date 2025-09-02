072d2525fc63216b21d024b004c448cb3bd345bd
Aryan Rathore <aryanrathoreop@gmail.com>
2025-09-02 12:00:00 +0530

    Add solution for LCS of three sequences

--- AlgorithmToolbox/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.cpp
+++ AlgorithmToolbox/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.cpp
@@ -3,10 +3,50 @@
 
 using std::vector;
 
-int lcs3(vector<int> &a, vector<int> &b, vector<int> &c) {
-  //write your code here
-  return std::min(std::min(a.size(), b.size()), c.size());
+vector<int> lcs3(vector<int> &a, vector<int> &b, vector<int> &c) {
+  vector<vector<int>> dp1(a.size()+1, vector<int>(b.size()+1, 0));
+  for(int i =1 ; i <= a.size(); i++){
+    for(int j = 1; j<= b.size(); j++){
+      if(a[i-1] == b[j-1]) dp1[i][j] = 1+dp1[i-1][j-1];
+      else dp1[i][j] = std::max(dp1[i-1][j], dp1[i][j-1]);
+    }
+    }
+    //Building the common sequence
+    int i = a.size(), j = b.size();
+    vector<int> commonOfAB;
+    while(i>0 && j>0){
+    if(a[i-1] == b[j-1]){
+      commonOfAB.push_back(a[i-1]);
+      i--;
+      j--;
+    }else if(dp1[i-1][j]>dp1[i][j-1]) i--;
+    else j--;
 }
+    reverse(commonOfAB.begin(), commonOfAB.end());
+    vector<vector<int>> dp2(c.size() +1,vector<int>(commonOfAB.size()+1, 0));
+
+      for(int k =1 ; k <= c.size(); k++){
+    for(int l = 1; l<= commonOfAB.size(); l++){
+      if(c[k-1] == commonOfAB[l-1]) dp2[k][l] = 1+dp2[k-1][l-1];
+      else dp2[k][l] = std::max(dp2[k-1][l], dp2[k][l-1]);
+    }
+    }
+    //Building the common sequence
+    int k = c.size(), l = commonOfAB.size();
+    vector<int> commonOfABC;
+    while(k>0 && l>0){
+    if(c[k-1] == commonOfAB[l-1]){
+      commonOfABC.push_back(c[k-1]);
+      k--;
+      l--;
+    }else if(dp2[k-1][l]>dp2[k][l-1]) k--;
+    else l--;
+  }
+  reverse(commonOfABC.begin(), commonOfABC.end());
+
+  return commonOfABC;
+}
+
 
 int main() {
   size_t an;
